febb333e7fb18402b877f61ed1b0dce7d584414f
Michael <m.gavrilov112@gmail.com>
2023-12-06 19:36:24 +0000

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+    // Set custom directories for input, output, includes, and data
+    eleventyConfig.addPassthroughCopy("css");
+
+    return {
+      passthroughFileCopy: true,
+
+      dir: {
+        input: "src",
+        includes: "_includes",
+        data: "_data",
+        output: "_site"
+        
+      }
+    
+    };
+   
+   };
